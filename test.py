fc792edc85f5a2a365f37afc3c739a852f466ce1
lfrenteriax <lfrenteriax@hotmail.com>
2023-07-09 02:56:22 -0500

    Update test.py

--- test.py
+++ test.py
@@ -4,31 +4,39 @@ import xbmcaddon
 import xbmcgui
 import os
 import xbmcvfs
+from contextlib import closing
+from xbmcvfs import File
+
 from downloader import *
 
-addon       = xbmcaddon.Addon()
-addonname   = addon.getAddonInfo('name')
-CWD = addon.getAddonInfo('path') # for kodi 19 and up..
 
 
-phd=os.path.join('special://userdata//lio2023')
-codePath=phd+"//code.txt"
-if not os.path.exists(phd):
-	xbmcvfs.mkdir(phd)
-	f=xbmcvfs.File(codePath,'w')
-	f.write("0000".encode("utf-8"))
+def upateTv(path):
+	f=xbmcvfs.File(path,'r')
+	data=f.read()
+	f.close()
+	import re
+	pat = r'.*?id="m3uPath">(.*)</setting>.*'             #See Note at the bottom of the answer
+	match = re.search(pat, data)
+	data=data.replace(match.group(1),chnPtd)
+	f=xbmcvfs.File(path,'w')
+	f.write(data)
 	f.close()
 
+def readFile(path):
+	
+	with closing(File(path)) as fo:
+		text = fo.read()
+		
+	return text
 
+def listFiles(path):
+	dirs, files = xbmcvfs.listdir(path)
+	data=""
+	for file in files:
+		data=data+file+"\n"
+	xbmcgui.Dialog().ok(path,data)
 
-usrPtd=phd+"//users.txt"
-#userUrl="https://public.db.files.1drv.com/y4mWcij-R_1AVnsV3_l6Zi_Q8Ml4qcBva452X0zy6x7U-svBbwR00bw_9xq1wVTGdZkVJvfZYl2J2taaU_2ap1oP00PCqm46z6hNiSbR1HrE3uCyNsoufU0Ws44R6hO-0gkiwgzCgJnkBGL6cRyduYG6t-cRMQ2LEyAbUUddsOc9_8kh3_exKeuKm3h8Bq2hCEklwIVQjHis7vbTeo0Kh0adP968YLHpMrfN2iScYQ5mGE?AVOverride=1"
-userUrl="https://raw.githubusercontent.com/lfrenteriax/kodiaddon/main/users.txt"
-chnUrl="https://raw.githubusercontent.com/lfrenteriax/channels/master/lio2023"
-chnPtd=phd+"//lio2023.m3u"
-teleUrl='https://tv.teleclub.xyz/activar'
-data = "submit=ACTIVAR%2BAHORA";
-headers= { 'Content-Type': 'application/x-www-form-urlencoded','referer': 'https://tv.teleclub.xyz/activar'}
 def debug(msg):
 
 	window = xbmcgui.Window(xbmcgui.getCurrentWindowId())
@@ -36,47 +44,98 @@ def debug(msg):
                                             font='font24_title', textColor='0xFFFFFF00')
 	window.addControl(label)
 
-def isActivado(code,usr):
-		f=xbmcvfs.File(code,'r')
-		code=f.read()
+
+
+
+def isActivado(cdP,usrP):
+
+		f=xbmcvfs.File(usrP,'r')
+		usuarios=f.read()
 		f.close()
-		f=xbmcvfs.File(usr,'r')
-		usr=f.read()
-		f.close()
-		
-		if(usr.split('\n').count(code)):
+		#xbmcgui.Dialog().ok("users",usuarios)
+		f1=xbmcvfs.File(cdP,'r')
+		codigo=f1.read()
+		f1.close()
+		#xbmcgui.Dialog().ok("codigo", codigo)
+		if(usuarios.split('\n').count(codigo)):
 			return 1
 		else:
 			return 0
+
+
+
+
+
+
+
+
+
+
+
+addon       = xbmcaddon.Addon()
+addonname   = addon.getAddonInfo('name')
+CWD = addon.getAddonInfo('path') # for kodi 19 and up..
+
+
+phd=os.path.join('special://userdata//lio2023')
+#usrPtd=os.path.join('special://userdata//lio2023//users.txt')
+
+phdTv=os.path.join('special://userdata//addon_data//pvr.iptvsimple//instance-settings-1.xml')
+codePath=phd+"//code.txt"
+usrPtd=phd+"//users.txt"
+cd=readFile(codePath)
+#xbmcgui.Dialog().ok("Cd",cd)
+
+if not cd :
+	#xbmcgui.Dialog().ok("Creando directorio",phd)
+	xbmcvfs.mkdir(phd)
+	f=xbmcvfs.File(codePath,'w')
+	f.write("0000".encode("utf-8"))
+	f.close()
+
+
+
+#usrPtd=phd+"//users.txt"
+#userUrl="https://public.db.files.1drv.com/y4mWcij-R_1AVnsV3_l6Zi_Q8Ml4qcBva452X0zy6x7U-svBbwR00bw_9xq1wVTGdZkVJvfZYl2J2taaU_2ap1oP00PCqm46z6hNiSbR1HrE3uCyNsoufU0Ws44R6hO-0gkiwgzCgJnkBGL6cRyduYG6t-cRMQ2LEyAbUUddsOc9_8kh3_exKeuKm3h8Bq2hCEklwIVQjHis7vbTeo0Kh0adP968YLHpMrfN2iScYQ5mGE?AVOverride=1"
+userUrl="https://raw.githubusercontent.com/lfrenteriax/kodiaddon/main/users.txt"
+chnUrl="https://raw.githubusercontent.com/lfrenteriax/channels/master/lio2023"
+chnPtd=phd+"//lio2023.m3u"
+teleUrl='https://tv.teleclub.xyz/activar'
+data = "submit=ACTIVAR%2BAHORA";
+headers= { 'Content-Type': 'application/x-www-form-urlencoded','referer': 'https://tv.teleclub.xyz/activar'}
+
+
+#listFiles(phd)
+
+#listFiles(phdTv)
+#readFile(usrPtd)
+#isActivado(codePath,usrPtd)
+#readFile(codePath)
 def main():
 	monitor = xbmc.Monitor()
-	nAtime=20
+	nAtime=5
 	aTime=3600
 	showOk=False
 	timeToWait=nAtime
 	download(chnUrl,chnPtd)
-	xbmcgui.Dialog().ok("inicio", CWD)
-
+	download(userUrl,usrPtd)
+		
+	#xbmcgui.Dialog().ok("inicio", "Presione para continuar")
 	while not monitor.abortRequested():
 		# Sleep/wait for abort for 10 seconds
 		if monitor.waitForAbort(timeToWait):
 			# Abort was requested while waiting. We should exit
 			
 			break
-		
-		timeToWait=timeToWait-1
+		#readFile(usrPtd)
+		#timeToWait=timeToWait-1
 		debug(str(timeToWait))
 		if timeToWait<10:
 			timeToWait=nAtime
-		
+		upateTv(phdTv)
 		if 1:
 		#try:
-			download(userUrl,usrPtd)
-			
 			if not isActivado(codePath,usrPtd):
-				f=xbmcvfs.File(chnPtd,'w')
-				f.write("".encode("utf-8"))
-				f.close()
 				ret = xbmcgui.Dialog().yesno('NO ACTIVADO', 'Quieres activarlos ahora?')
 				if ret:
 					d = xbmcgui.Dialog().numeric(0, 'Ingrese el codigo de activaciòn')
